75a1f6fc0dbf8cf8c67e0b3473bfeba869ccb369
zxBIB Lechón,Miguel (MED BDS) EXTERNAL <miguel.lechon.ext@boehringer-ingelheim.com>
2025-02-13 14:15:46 +0100

    Add missing `afmm_param` argument when calling `listings_server`.

--- R/check_call_auto.R
+++ R/check_call_auto.R
@@ -4,7 +4,7 @@
 
 # dv.listings::mod_listings
 check_mod_listings_auto <- function(afmm, datasets, module_id, dataset_names, default_vars, pagination,
-    intended_use_label, warn, err) {
+    intended_use_label, subjid_var, receiver_id, warn, err) {
     OK <- logical(0)
     used_dataset_names <- new.env(parent = emptyenv())
     OK[["module_id"]] <- CM$check_module_id("module_id", module_id, warn, err)
@@ -12,13 +12,19 @@ check_mod_listings_auto <- function(afmm, datasets, module_id, dataset_names, de
     OK[["dataset_names"]] <- CM$check_dataset_name("dataset_names", dataset_names, flags, datasets, used_dataset_names,
         warn, err)
     "NOTE: default_vars (group) has no associated automated checks"
-    "      The expectation is that it does not require one or that"
+    "      The expectation is that it either does not require them or that"
     "      the caller of this function has written manual checks near the call site."
     "NOTE: pagination (group) has no associated automated checks"
-    "      The expectation is that it does not require one or that"
+    "      The expectation is that it either does not require them or that"
     "      the caller of this function has written manual checks near the call site."
     "NOTE: intended_use_label (group) has no associated automated checks"
-    "      The expectation is that it does not require one or that"
+    "      The expectation is that it either does not require them or that"
+    "      the caller of this function has written manual checks near the call site."
+    "NOTE: subjid_var (group) has no associated automated checks"
+    "      The expectation is that it either does not require them or that"
+    "      the caller of this function has written manual checks near the call site."
+    "NOTE: receiver_id (group) has no associated automated checks"
+    "      The expectation is that it either does not require them or that"
     "      the caller of this function has written manual checks near the call site."
     return(OK)
 }
